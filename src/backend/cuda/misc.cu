77cbd75dc272262c8d9f495ddc80cd083d0eb592
interestingLSY <interestingLSY@gmail.com>
2023-12-29 07:54:19 +0800

    Add fused adam kernel

--- src/backend/cuda/misc.cu
+++ src/backend/cuda/misc.cu
@@ -22,10 +22,16 @@ __global__ void get_correct_sample_count_kernel(
 ) {
 	int64_t batch_id = blockIdx.x;
 	int64_t class_id = threadIdx.x;
-	T my_pred_output = pred_output[INDEX_2D(batch_size, num_classes, batch_id, class_id)];
 
-	T group_max_pred_output = block_reduce_max_broadcast<double>(my_pred_output);
-	if (group_max_pred_output == my_pred_output && ground_truth[batch_id] == class_id) {
+	T my_pred_output = pred_output[INDEX_2D(batch_size, num_classes, batch_id, class_id)];
+	T group_max_pred_output = block_reduce_max_broadcast<T>(my_pred_output);
+
+	int32_t my_max_pos = (my_pred_output == group_max_pred_output) ? class_id : -1;
+	int32_t group_max_pos = block_reduce_max_broadcast<int32_t>(my_max_pos);
+
+	if (group_max_pred_output == my_pred_output &&
+		group_max_pos == class_id &&
+		ground_truth[batch_id] == class_id) {
 		atomicAdd(&answer, 1);
 	}
 }
@@ -64,7 +70,7 @@ __global__ void sgd_grad_update_kernel(
 	T weight_decay,
 	int64_t num_elements
 ) {
-	#pragma unroll 2
+	#pragma unroll 4
 	for (int64_t i = blockIdx.x * blockDim.x + threadIdx.x; i < num_elements; i += blockDim.x * gridDim.x) {
 		T cur_grad = HAVE_WEIGHT_DECAY ? grad[i] + weight_decay * weight[i] : grad[i];
 		if constexpr (HAVE_MOMENTUM) {
@@ -103,6 +109,77 @@ void sgd_grad_update(Tensor &weight, const Tensor &grad, Tensor &momentum, doubl
 			DISPATCH(true, true);
 		}
 	}
+}
+
+template<typename T>
+__device__ __forceinline__ T my_sqrt(const T x) {
+	if constexpr (std::is_same<T, half>::value) {
+		return (T)sqrtf((float)x);
+	} else if constexpr (std::is_same<T, float>::value) {
+		return sqrtf(x);
+	} else if constexpr (std::is_same<T, double>::value) {
+		return sqrt(x);
+	} else {
+		assert(false);
+	}
+}
+
+template<typename T>
+__device__ __forceinline__ T my_pow(const T x, const T y) {
+	if constexpr (std::is_same<T, half>::value) {
+		return (T)powf((float)x, (float)y);
+	} else if constexpr (std::is_same<T, float>::value) {
+		return powf(x, y);
+	} else if constexpr (std::is_same<T, double>::value) {
+		return pow(x, y);
+	} else {
+		assert(false);
+	}
+}
+
+template<typename T>
+__global__ void adam_grad_update_kernel(
+	T* __restrict__ weight,
+	const T* __restrict__ grad,
+	T* __restrict__ momentum,
+	T* __restrict__ velocity,
+	T learning_rate,
+	T beta1,
+	T beta2,
+	T eps,
+	int64_t cur_timestamp,
+	int64_t num_elements
+) {
+	T cur_timestamp_T = (T)(long long)cur_timestamp;
+	#pragma unroll 4
+	for (int64_t i = blockIdx.x * blockDim.x + threadIdx.x; i < num_elements; i += blockDim.x * gridDim.x) {
+		T cur_grad = grad[i];
+		momentum[i] = beta1 * momentum[i] + ((T)1. - beta1) * cur_grad;
+		velocity[i] = beta2 * velocity[i] + ((T)1. - beta2) * cur_grad * cur_grad;
+		T adjusted_momentum = momentum[i] / ((T)1. - my_pow(beta1, cur_timestamp_T));
+		T adjusted_velocity = velocity[i] / ((T)1. - my_pow(beta2, cur_timestamp_T));
+		weight[i] = weight[i] - learning_rate * adjusted_momentum / (my_sqrt(adjusted_velocity) + eps);
+	}
+}
+
+void adam_grad_update(Tensor &weight, const Tensor &grad, Tensor &momentum, Tensor &velocity, int cur_timestamp, double learning_rate, double beta1, double beta2, double eps) {
+	int64_t numel = weight.numel();
+	int64_t block_size = ELEMENT_WISE_KERNEL_BLOCK_SIZE;
+	int64_t grid_size = element_wise_kernel_get_num_grids(numel);
+	DISPATCH_ON_DTYPE_CUDA_BACKEND(weight.dtype,
+		adam_grad_update_kernel<T><<<grid_size, block_size>>>(
+			(T*) weight.data_ptr(),
+			(const T*) grad.data_ptr(),
+			(T*) momentum.data_ptr(),
+			(T*) velocity.data_ptr(),
+			(T) learning_rate,
+			(T) beta1,
+			(T) beta2,
+			(T) eps,
+			cur_timestamp,
+			numel
+		)
+	);
 }
 
 }
--- src/backend/cuda/misc.h
+++ src/backend/cuda/misc.h
@@ -10,4 +10,6 @@ int64_t get_correct_sample_count(const Tensor &output, const Tensor &ground_trut
 
 void sgd_grad_update(Tensor &weight, const Tensor &grad, Tensor &momentum, double learning_rate, double momentum_factor, double weight_decay);
 
+void adam_grad_update(Tensor &weight, const Tensor &grad, Tensor &momentum, Tensor &velocity, int cur_timestamp, double learning_rate, double beta1, double beta2, double eps);
+
 }
